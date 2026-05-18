8cc30c7ea8da45a5214d8f1dd9b1b8ed35110529
Karina Ikhsanova <karina23twins@yandex.ru>
2026-05-19 01:59:16 +0300

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,30 +15,124 @@ const (
 
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
-	// ваш код здесь
+	if size <= 0 {
+		log.Println("size of the slice must be greater than zero")
+		return nil
+	}
+
+	src := rand.NewSource(time.Now().Unix())
+	rng := rand.New(src)
+	slice := make([]int, size)
+
+	for i := 0; i < size; i++ {
+		randomNumber := rng.Int()
+
+		slice[i] = randomNumber
+	}
+	return slice
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
-	// ваш код здесь
+
+	if len(data) == 0 {
+		log.Println("slice is empty")
+		return 0
+	}
+
+	maxNumber := data[0]
+	for _, num := range data {
+		if num > maxNumber {
+			maxNumber = num
+		}
+	}
+
+	if maxNumber < 0 {
+		log.Println("slice of negative numbers was generated")
+		return 0
+	}
+	return maxNumber
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
-	// ваш код здесь
+	var wg sync.WaitGroup
+
+	var chunkSize int
+	var chunks int
+
+	sliceOfMax := make([]int, CHUNKS)
+
+	if len(data) == 0 {
+		log.Println("slice is empty")
+		return 0
+	}
+
+	if len(data) == 1 {
+		return maximum(data)
+	}
+
+	if len(data)%CHUNKS == 0 {
+		chunkSize = len(data) / CHUNKS
+		chunks = CHUNKS
+	} else {
+		for i := 2; i < SIZE; i++ {
+			if len(data)%i == 0 {
+				chunkSize = len(data) / i
+				chunks = i
+				break
+			}
+		}
+	}
+
+	for i := 0; i < chunks; i++ {
+		index := i * chunkSize
+		chunk := data[index:(index + chunkSize)]
+
+		wg.Add(1)
+
+		go func(chunk []int) {
+			defer wg.Done()
+
+			max := maximum(chunk)
+			sliceOfMax = append(sliceOfMax, max)
+		}(chunk)
+	}
+	wg.Wait()
+
+	maxOfMax := maximum(sliceOfMax)
+	return maxOfMax
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
-	// ваш код здесь
+	if SIZE <= 0 {
+		log.Println("slice size must be greater than zero")
+		return
+	}
+
+	if CHUNKS <= 0 {
+		log.Println("value of CHUNKS must be greater than zero")
+		return
+	}
+
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
+	slice := generateRandomElements(SIZE)
+	if len(slice) == 0 {
+		log.Println("slice is empty")
+		return
+	}
 
 	fmt.Println("Ищем максимальное значение в один поток")
-	// ваш код здесь
+	start := time.Now()
+	max := maximum(slice)
+	elapsed := time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
+	fmt.Printf("Ищем максимальное значение в %d потоков\n", CHUNKS)
+	start = time.Now()
+	max = maxChunks(slice)
+	elapsed = time.Since(start).Microseconds()
 
 	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,45 @@
 package main
 
-// Пишите тесты в этом файле
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateRandomElements(t *testing.T) {
+
+	testSlice := generateRandomElements(-5)
+	require.Nil(t, testSlice)
+
+	testSlice = generateRandomElements(SIZE)
+	assert.Len(t, testSlice, SIZE)
+	assert.NotNil(t, testSlice)
+}
+
+func TestMaximumAndMaxChunks(t *testing.T) {
+
+	testStruct := []struct {
+		slice  []int
+		result int
+	}{
+		{slice: []int{10, 20, 30}, result: 30},
+		{slice: []int{-50, 45}, result: 45},
+		{slice: []int{7, 0, 777, 7777}, result: 7777},
+		{slice: []int{}, result: 0},
+		{slice: []int{1}, result: 1},
+		{slice: []int{0}, result: 0},
+		{slice: []int{-1}, result: 0},
+		{slice: []int{-1, -10, -100}, result: 0},
+	}
+
+	for _, s := range testStruct {
+		max := maximum(s.slice)
+		assert.Equal(t, s.result, max)
+
+		maximum := maxChunks(s.slice)
+		assert.Equal(t, s.result, maximum)
+
+		assert.Equal(t, max, maximum)
+	}
+}
